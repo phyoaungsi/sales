c79887d081945a16c4a4a51e65b6ec85e4df2643
phyo <phyoaungsiz@gmail.com>
2018-03-07 20:50:18 +0800

    Refactoring

--- src/parkrite-app/src/main/java/sg/edu/nus/iss/pmprs/dao/CarParkRepository.java
+++
@@ -1,9 +0,0 @@
-package sg.edu.nus.iss.pmprs.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import sg.edu.nus.iss.pmprs.entity.CarPark;
-import sg.edu.nus.iss.pmprs.entity.Product;
-
-public interface CarParkRepository extends JpaRepository<Product ,Integer>{
-}
